27f64df910b336908a64c11630b7faaf3fa95e52
mithil <mithil@mithil>
2016-12-21 10:11:29 +1100

    Initial COmmit

---
+++ AwsLambdaThumbnailS3/src/com/studytrails/aws/lambda/S3Thumbnail.java
@@ -0,0 +1,12 @@
+package com.studytrails.aws.lambda;
+
+import com.amazonaws.services.lambda.runtime.Context;
+import com.amazonaws.services.lambda.runtime.events.S3Event;
+
+public class S3Thumbnail {
+
+	void createThumbnail(S3Event event, Context context) {
+
+	}
+
+}
