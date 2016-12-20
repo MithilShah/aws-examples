82b2b3ac5bae6aad0c7041f03c5855faa45085ba
mithil <mithil@mithil>
2016-12-21 10:29:19 +1100

    shade

--- AwsLambdaThumbnailS3/src/com/studytrails/aws/lambda/S3Thumbnail.java
+++ AwsLambdaThumbnailS3/src/com/studytrails/aws/lambda/S3Thumbnail.java
@@ -1,12 +1,24 @@
 package com.studytrails.aws.lambda;
 
+import java.util.List;
+
 import com.amazonaws.services.lambda.runtime.Context;
+import com.amazonaws.services.lambda.runtime.LambdaLogger;
 import com.amazonaws.services.lambda.runtime.events.S3Event;
+import com.amazonaws.services.s3.event.S3EventNotification.S3Entity;
+import com.amazonaws.services.s3.event.S3EventNotification.S3EventNotificationRecord;
+import com.amazonaws.services.s3.event.S3EventNotification.S3ObjectEntity;
 
 public class S3Thumbnail {
 
 	void createThumbnail(S3Event event, Context context) {
-
+		LambdaLogger logger = context.getLogger();
+		List<S3EventNotificationRecord> eventRecords = event.getRecords();
+		for (S3EventNotificationRecord record : eventRecords) {
+			S3Entity entity = record.getS3();
+			S3ObjectEntity objectEntity = entity.getObject();
+			logger.log(objectEntity.getKey());
+		}
 	}
 
 }
